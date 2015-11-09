ccdf246ccae733542d577db3f38629043233f198
chihwan <clghks@quicket.co.kr>
2015-11-09 15:13:25 +0900

    Build Environment Data Binding

--- DataBinding/app/src/main/java/com/clghks/databinding/MainActivity.java
+++ DataBinding/app/src/main/java/com/clghks/databinding/MainActivity.java
@@ -1,13 +1,20 @@
 package com.clghks.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.clghks.databinding.data.User;
+import com.clghks.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
---
+++ DataBinding/app/src/main/java/com/clghks/databinding/data/User.java
@@ -0,0 +1,19 @@
+package com.clghks.databinding.data;
+
+/**
+ * Created by chihwan on 15. 11. 9..
+ */
+public class User {
+    private final String firstName;
+    private final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+}
